ca383297d7c517570d500eafb252bf694805c80a
Nicholas Garner <garner104@gmail.com>
2018-10-07 21:15:30 +1000

    Connected cassandra to express backend

---
+++ rest_api/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+const cassandra = require('cassandra-driver');
+
+const client = new cassandra.Client({ contactPoints: ['cassandra'], keyspace: 'users'});
+
+const app = express();
+app.get('/', (req, res) => {
+  console.log();
+  const query = 'SELECT * from users.login';
+  client.execute(query)
+    .then(result => {
+      const users = result.rows.map(row => row.id);
+      res.json(users);
+    })
+    .catch(err => {
+      res.json(err);
+    })
+});
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
